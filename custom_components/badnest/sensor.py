f0b36de777d7d394ca12599d324f5f5046d707f4
77degrees <80057302+77degrees@users.noreply.github.com>
2024-06-19 03:42:31 -0500

    Update sensor.py

--- custom_components/badnest/sensor.py
+++ custom_components/badnest/sensor.py
@@ -1,58 +1,37 @@
 import logging
 
+from homeassistant.const import UnitOfTemperature
 from homeassistant.helpers.entity import Entity
+import homeassistant.util.temperature as temperature_util
 
 from .const import DOMAIN
 
-from homeassistant.const import (
-    ATTR_BATTERY_LEVEL,
-    DEVICE_CLASS_TEMPERATURE,
-    TEMP_CELSIUS
-)
-
 _LOGGER = logging.getLogger(__name__)
 
-PROTECT_SENSOR_TYPES = [
-    "co_status",
-    "smoke_status",
-    "battery_health_state"
-]
-
-
-async def async_setup_platform(hass,
-                               config,
-                               async_add_entities,
-                               discovery_info=None):
-    """Set up the Nest climate device."""
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the Nest sensors."""
     api = hass.data[DOMAIN]['api']
 
-    temperature_sensors = []
-    _LOGGER.info("Adding temperature sensors")
-    for sensor in api['temperature_sensors']:
-        _LOGGER.info(f"Adding nest temp sensor uuid: {sensor}")
-        temperature_sensors.append(NestTemperatureSensor(sensor, api))
+    sensors = []
+    _LOGGER.info("Adding sensors")
+    for sensor in api['sensors']:
+        _LOGGER.info(f"Adding nest sensor uuid: {sensor}")
+        sensors.append(NestSensor(sensor, api))
 
-    async_add_entities(temperature_sensors)
+    async_add_entities(sensors)
 
-    protect_sensors = []
-    _LOGGER.info("Adding protect sensors")
-    for sensor in api['protects']:
-        _LOGGER.info(f"Adding nest protect sensor uuid: {sensor}")
-        for sensor_type in PROTECT_SENSOR_TYPES:
-            protect_sensors.append(NestProtectSensor(sensor, sensor_type, api))
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return temperature_util.celsius_to_fahrenheit(celsius)
 
-    async_add_entities(protect_sensors)
-
-
-class NestTemperatureSensor(Entity):
-    """Implementation of the Nest Temperature Sensor."""
+class NestSensor(Entity):
+    """Representation of a Nest sensor."""
 
     def __init__(self, device_id, api):
         """Initialize the sensor."""
-        self._name = "Nest Temperature Sensor"
-        self._unit_of_measurement = TEMP_CELSIUS
         self.device_id = device_id
         self.device = api
+        self._unit_of_measurement = UnitOfTemperature.FAHRENHEIT
 
     @property
     def unique_id(self):
@@ -61,63 +40,27 @@ class NestTemperatureSensor(Entity):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return a friendly name."""
         return self.device.device_data[self.device_id]['name']
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self.device.device_data[self.device_id]['temperature']
-
-    @property
-    def device_class(self):
-        """Return the device class of this entity."""
-        return DEVICE_CLASS_TEMPERATURE
+        temp_celsius = self.device.device_data[self.device_id]['current_temperature']
+        if self._unit_of_measurement == UnitOfTemperature.FAHRENHEIT:
+            return celsius_to_fahrenheit(temp_celsius)
+        return temp_celsius
 
     @property
     def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
+        """Return the unit of measurement."""
         return self._unit_of_measurement
 
-    def update(self):
-        """Get the latest data from the DHT and updates the states."""
-        self.device.update()
-
     @property
-    def device_state_attributes(self):
-        """Return the state attributes."""
-        return {
-            ATTR_BATTERY_LEVEL:
-                self.device.device_data[self.device_id]['battery_level']
-        }
-
-
-class NestProtectSensor(Entity):
-    """Implementation of the Nest Protect sensor."""
-
-    def __init__(self, device_id, sensor_type, api):
-        """Initialize the sensor."""
-        self._name = "Nest Protect Sensor"
-        self.device_id = device_id
-        self._sensor_type = sensor_type
-        self.device = api
-
-    @property
-    def unique_id(self):
-        """Return an unique ID."""
-        return self.device_id + '_' + self._sensor_type
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self.device.device_data[self.device_id]['name'] + \
-            f' {self._sensor_type}'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self.device.device_data[self.device_id][self._sensor_type]
+    def device_class(self):
+        """Return the device class."""
+        return "temperature"
 
     def update(self):
-        """Get the latest data from the Protect and updates the states."""
+        """Fetch new state data for the sensor."""
         self.device.update()
